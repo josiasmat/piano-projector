e733400fb952fdc67bf51512a87ecc36684c73fd
Josias Matschulat <josiasmatschulat@outlook.com>
2025-10-24 16:02:37 -0300

    New build script

---
+++ build.js
@@ -0,0 +1,58 @@
+import { argv } from 'node:process';
+import * as esbuild from 'esbuild';
+
+const productionMode = ('--dev' !== (argv[2] || process.env.NODE_ENV));
+const targetBrowsers = ['chrome130','firefox132'];
+
+console.log(`${ productionMode ? 'production' : 'development' } build`);
+
+// bundle CSS
+const buildCSS = await esbuild.context({
+    entryPoints: [ './src/css/main.css' ],
+    bundle: true,
+    format: 'esm',
+    target: targetBrowsers,
+    loader: {
+        '.css': 'css'
+    },
+    logLevel: productionMode ? 'error' : 'info',
+    minify: productionMode,
+    sourcemap: !productionMode,
+    outfile: './pub/bundle.css'
+});
+
+// bundle JS
+const buildJS = await esbuild.context({
+    entryPoints: [ './src/js/main.js' ],
+    format: 'esm',
+    bundle: true,
+    target: targetBrowsers,
+    drop: productionMode ? ['debugger', 'console'] : [],
+    logLevel: productionMode ? 'error' : 'info',
+    minify: productionMode,
+    sourcemap: !productionMode,
+    outfile: './pub/bundle.js'
+});
+
+
+if (productionMode) {
+
+  // single production build
+  await buildCSS.rebuild();
+  buildCSS.dispose();
+
+  await buildJS.rebuild();
+  buildJS.dispose();
+
+} else {
+
+  // watch for file changes
+  await buildCSS.watch();
+  await buildJS.watch();
+
+  // development server
+  await buildCSS.serve({
+    servedir: '.'
+  });
+
+}
