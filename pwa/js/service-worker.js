8c8bbfbb8919393242ac0b51fa550e54a505e350
Josias Matschulat <josiasmatschulat@outlook.com>
2025-11-23 15:11:11 -0300

    Implement service worker for caching

---
+++ pwa/js/service-worker.js
@@ -0,0 +1,41 @@
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = [
+    '../', 
+    '../index.html', 
+    '../bundle.css', 
+    '../bundle.js', 
+    '../manifest.json',
+    '../assets/icon/favicon.svg',
+    '../assets/icon/favicon.png',
+    '../assets/icon/favicon-192.png',
+    '../assets/icon/favicon-512.png',
+    '../assets/svg/lped.svg',
+    '../assets/svg/mped.svg',
+    '../assets/svg/rped.svg',
+    '../assets/svg/midikbd.svg'
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
