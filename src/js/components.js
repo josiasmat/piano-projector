c488e98fb507369d29ba573234c6854750383e92
Josias Matschulat <josiasmatschulat@outlook.com>
2026-02-07 13:35:55 -0300

    Moved Shoelace imports to components.js

---
+++ src/js/components.js
@@ -0,0 +1,24 @@
+// Shoelace localization files
+import '@shoelace-style/shoelace/dist/translations/en.js';
+import '@shoelace-style/shoelace/dist/translations/pt.js';
+
+// Shoelace components
+import '@shoelace-style/shoelace/dist/components/badge/badge.js';
+import '@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/button-group/button-group.js';
+import '@shoelace-style/shoelace/dist/components/color-picker/color-picker.js';
+import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
+import '@shoelace-style/shoelace/dist/components/divider/divider.js';
+import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/menu/menu.js';
+import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
+import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
+import '@shoelace-style/shoelace/dist/components/visually-hidden/visually-hidden.js';
+
+// Shoelace: set base path
+import { setBasePath as shoelaceSetBaseBath } 
+    from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+shoelaceSetBaseBath('assets/shoelace');
